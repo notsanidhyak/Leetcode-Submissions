0d6fe3e778bf80f67034e43ad86c7436737ade6d
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-05-30 16:53:50 +0530

    Time: 5 ms (67.68%) | Memory: 44.4 MB (67.32%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,89 @@
+// DFS Approach -
+
+// class Solution {
+//     public boolean checkCycle(int node, List<List<Integer>> adj, boolean[] visited, boolean [] dfsvisited) {
+//         visited[node] = true;
+//         dfsvisited[node] = true;
+
+//         for (int adjNode : adj.get(node)){
+//             if (!visited[adjNode]){
+//                 if(checkCycle(adjNode, adj, visited, dfsvisited)){
+//                     return true;
+//                 }
+//             }
+//             else if(dfsvisited[adjNode]){
+//                 return true;
+//             }
+//         }
+//         dfsvisited[node] = false;
+//         return false;
+//     }
+
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
+//         boolean visited [] = new boolean[numCourses];
+//         boolean dfsvisited [] = new boolean[numCourses];
+
+//         List<List<Integer>> adj = new ArrayList<>();
+//         for (int i = 0; i < numCourses; i++) {
+//             adj.add(new ArrayList<>());
+//         }
+
+//         for (int[] pair : prerequisites) {
+//             int course = pair[0];
+//             int pre = pair[1];
+//             adj.get(pre).add(course);
+//         }
+
+//         for (int i = 0; i < numCourses; i++) {
+//             if (!visited[i]) {
+//                 if (checkCycle(i, adj, visited, dfsvisited)) {
+//                     return false; // Cycle found, cannot finish courses
+//                 }
+//             }
+//         }
+//         return true; // No cycles found, can finish courses
+//     }
+// }
+
+// ------------------------------------------------------------------
+
+// BFS Approach (aka Kahn's algo - topo sort method)
+// if len(topo)<V -> cycle detected
+// because no further indegree == 0 found, hence stopped
+
 class Solution {
-    // Method to check if there's a cycle starting from the node `i`
-    public boolean checkCycle(int node, List<List<Integer>> adj, boolean[] visited, boolean [] dfsvisited) {
-        visited[node] = true;
-        dfsvisited[node] = true;
-
-        for (int adjNode : adj.get(node)){
-            if (!visited[adjNode]){
-                if(checkCycle(adjNode, adj, visited, dfsvisited)){
-                    return true;
-                }
-            }
-            else if(dfsvisited[adjNode]){
-                return true;
-            }
-        }
-        dfsvisited[node] = false;
-        return false;
-    }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean visited [] = new boolean[numCourses];
-        boolean dfsvisited [] = new boolean[numCourses];
-
+        int [] indegree = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+        for(int i = 0; i<numCourses; i++){
             adj.add(new ArrayList<>());
         }
-
-        for (int[] pair : prerequisites) {
+        for (int [] pair : prerequisites){
             int course = pair[0];
             int pre = pair[1];
+            indegree[course]++;
             adj.get(pre).add(course);
         }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (checkCycle(i, adj, visited, dfsvisited)) {
-                    return false; // Cycle found, cannot finish courses
+        Queue <Integer> q = new LinkedList<>();
+        for(int i = 0; i<numCourses; i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int ct = 0;
+        while(!q.isEmpty()){
+            int node = q.remove();
+            ct++;
+            for(int adjNode : adj.get(node)){
+                indegree[adjNode]--;
+                if(indegree[adjNode]==0){
+                    q.add(adjNode);
                 }
             }
         }
-        return true; // No cycles found, can finish courses
+        if(ct==numCourses){
+            return true;
+        }
+        return false;
     }
 }
