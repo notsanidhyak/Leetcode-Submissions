3578a2ac841063875eb1580ad1b4f955489c5486
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-05-31 17:00:00 +0530

    Time: 5 ms (26.19%) | Memory: 52.1 MB (11.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,22 +1,54 @@
+// BFS Approach -
+
+// class Solution {
+//     public void bfs(char[][] grid, int i, int j){
+//         int [][] del = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+//         grid[i][j] = '2';
+//         int [] pair = {i, j};
+//         Queue<int []> q = new LinkedList<>();
+//         q.add(pair);
+//         while(!q.isEmpty()){
+//             int row = q.peek()[0];
+//             int col = q.peek()[1];
+//             q.remove();
+//             for (int [] delpair : del){
+//                 int newrow = row + delpair[0];
+//                 int newcol = col + delpair[1];
+//                 if(newrow >=0 && newrow<grid.length && newcol>=0 && newcol<grid[0].length && grid[newrow][newcol]=='1'){
+//                     grid[newrow][newcol]='2';
+//                     int [] newpair = {newrow, newcol};
+//                     q.add(newpair); 
+//                 }
+//             }
+//         }
+//     }
+//     public int numIslands(char[][] grid) {
+//         int ct = 0;
+//         for(int i = 0; i<grid.length; i++){
+//             for(int j = 0; j<grid[0].length; j++){
+//                 if (grid[i][j]=='1'){
+//                     bfs(grid, i, j);
+//                     ct++;
+//                 }
+//             }
+//         }
+//         return ct;
+//     }
+// }
+
+// ------------------------------------------------------------------
+
+// DFS Approach -
+
 class Solution {
-    public void bfs(char[][] grid, int i, int j){
+    public void dfs(char[][] grid, int i, int j){
         int [][] del = {{0,1}, {0,-1}, {-1,0}, {1,0}};
         grid[i][j] = '2';
-        int [] pair = {i, j};
-        Queue<int []> q = new LinkedList<>();
-        q.add(pair);
-        while(!q.isEmpty()){
-            int row = q.peek()[0];
-            int col = q.peek()[1];
-            q.remove();
-            for (int [] delpair : del){
-                int newrow = row + delpair[0];
-                int newcol = col + delpair[1];
-                if(newrow >=0 && newrow<grid.length && newcol>=0 && newcol<grid[0].length && grid[newrow][newcol]=='1'){
-                    grid[newrow][newcol]='2';
-                    int [] newpair = {newrow, newcol};
-                    q.add(newpair); 
-                }
+        for(int [] delpair : del){
+            int newrow = i + delpair[0];
+            int newcol = j + delpair[1];
+            if(newrow >=0 && newrow<grid.length && newcol>=0 && newcol<grid[0].length && grid[newrow][newcol]=='1'){
+                dfs(grid, newrow, newcol);
             }
         }
     }
@@ -25,7 +57,7 @@ class Solution {
         for(int i = 0; i<grid.length; i++){
             for(int j = 0; j<grid[0].length; j++){
                 if (grid[i][j]=='1'){
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                     ct++;
                 }
             }
