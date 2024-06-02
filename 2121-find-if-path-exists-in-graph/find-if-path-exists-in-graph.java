f6f6c7e3e4aa421ca5919efca5dbda4b9666fcaf
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-02 18:15:13 +0530

    Time: 118 ms (45.88%) | Memory: 161.2 MB (33.92%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,3 +1,36 @@
+// Visited means there's a path to reach and logic of allPossibleTravelNotDone -> Logic is correct but gives TLE!
+
+// class Solution {
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         if (edges.length == 0 && n==1){
+//             return true;
+//         }
+//         int [] visited = new int[n];
+//         boolean allPossibleTravelNotDone = true;
+//         visited[source] = 1;
+//         while(allPossibleTravelNotDone){
+//             // I assume that all Possible Travel is Done.
+//             allPossibleTravelNotDone = false;
+
+//             // Now i check pair from edges and if really all Possible Travel is Done then for every entry pair[0]=pair[1] and hence allPossibleTravelNotDone = true koi nahi karega
+//             for (int [] pair : edges){
+//                 if(visited[pair[0]] != visited[pair[1]]){
+//                     visited[pair[0]]=1;
+//                     visited[pair[1]]=1;
+//                     // Since all Possible Travel was Not Done
+//                     allPossibleTravelNotDone = true;
+//                 }
+//                 if(visited[destination]==1){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// -----------------------------------------------------------------
+
 // BFS Approach -
 
 // class Solution {
