d3b297110a9b69e210a2fc03ff3b9cd5fc5cad76
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-02 17:56:52 +0530

    Time: 152 ms (34.71%) | Memory: 161.1 MB (33.97%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,60 +1,6 @@
-// BFS Approach
-class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if (edges.length == 0 && n==1){
-            return true;
-        }
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i<n; i++){
-            adj.add(new ArrayList<>());
-        }
-        for (int [] pair : edges){
-            int a = pair[0];
-            int b = pair[1];
-            // Since undirected
-            adj.get(a).add(b);
-            adj.get(b).add(a);
-        }
-        int visited[] = new int [n];
-        Queue <Integer> q = new LinkedList<>();
-        visited[source] = 1;
-        q.add(source);
-        while(!q.isEmpty()){
-            int currnode = q.remove();
-            for(int adjNode : adj.get(currnode)){
-                if (adjNode == destination){
-                    return true;
-                }
-                else if (visited[adjNode]==0){
-                    visited[adjNode]=1;
-                    q.add(adjNode);
-                }
-            }
-        }
-        return false;
-    }
-}
-
-// ------------------------------------------------------------------
-
-// DFS Approach -
+// BFS Approach -
 
 // class Solution {
-//     public boolean dfsCheck(int currnode, int destination, List<List<Integer>> adj, int [] visited){
-//         visited[currnode] = 1;
-//         for(int adjNode : adj.get(currnode)){
-//             if (adjNode == destination){
-//                 return true;
-//             }
-//             else if(visited[adjNode]==0){
-//                 if(dfsCheck(adjNode, destination, adj, visited)){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-
 //     public boolean validPath(int n, int[][] edges, int source, int destination) {
 //         if (edges.length == 0 && n==1){
 //             return true;
@@ -71,9 +17,64 @@ class Solution {
 //             adj.get(b).add(a);
 //         }
 //         int visited[] = new int [n];
-//         if (dfsCheck(source, destination, adj, visited)){
-//             return true;
+//         Queue <Integer> q = new LinkedList<>();
+//         visited[source] = 1;
+//         q.add(source);
+//         while(!q.isEmpty()){
+//             int currnode = q.remove();
+//             for(int adjNode : adj.get(currnode)){
+//                 if (adjNode == destination){
+//                     return true;
+//                 }
+//                 else if (visited[adjNode]==0){
+//                     visited[adjNode]=1;
+//                     q.add(adjNode);
+//                 }
+//             }
 //         }
 //         return false;
 //     }
 // }
+
+// ------------------------------------------------------------------
+
+// DFS Approach -
+
+class Solution {
+    public boolean dfsCheck(int currnode, int destination, List<List<Integer>> adj, int [] visited){
+        if(currnode == destination){
+            return true;
+        }
+        visited[currnode] = 1;
+        for(int adjNode : adj.get(currnode)){
+            if(visited[adjNode]==0){
+                if(dfsCheck(adjNode, destination, adj, visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (edges.length == 0 && n==1){
+            return true;
+        }
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for (int [] pair : edges){
+            int a = pair[0];
+            int b = pair[1];
+            // Since undirected
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        int visited[] = new int [n];
+        if (dfsCheck(source, destination, adj, visited)){
+            return true;
+        }
+        return false;
+    }
+}
