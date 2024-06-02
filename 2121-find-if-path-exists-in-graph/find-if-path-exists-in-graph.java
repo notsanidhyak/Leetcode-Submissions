a62f4aa105bf2f7104d66d9f8a77f3745771df26
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-02 17:50:37 +0530

    Time: 163 ms (33.19%) | Memory: 160.6 MB (34.14%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean dfsCheck(int currnode, int destination, List<List<Integer>> adj, int [] visited){
+        visited[currnode] = 1;
+        for(int adjNode : adj.get(currnode)){
+            if (adjNode == destination){
+                return true;
+            }
+            else if(visited[adjNode]==0){
+                if(dfsCheck(adjNode, destination, adj, visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (edges.length == 0 && n==1){
+            return true;
+        }
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for (int [] pair : edges){
+            int a = pair[0];
+            int b = pair[1];
+            // Since undirected
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        int visited[] = new int [n];
+        if (dfsCheck(source, destination, adj, visited)){
+            return true;
+        }
+        return false;
+    }
+}
