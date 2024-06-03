d90bb0a9190e88e5878dc7cd16a130f402c96c58
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-03 14:56:42 +0530

    Time: 7 ms (51.85%) | Memory: 45.5 MB (13.88%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
@@ -1,16 +1,30 @@
+// class Solution {
+//     public int appendCharacters(String s, String t) {
+//         int i = 0; // pointer for s
+//         int j = 0; // pointer for t
+//         while(i<s.length() && j<t.length()){
+//             if(s.charAt(i)==t.charAt(j)){
+//                 i++;
+//                 j++;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return t.length()-j;
+//     }
+// }
+
 class Solution {
     public int appendCharacters(String s, String t) {
         int i = 0; // pointer for s
         int j = 0; // pointer for t
         while(i<s.length() && j<t.length()){
             if(s.charAt(i)==t.charAt(j)){
-                i++;
                 j++;
             }
-            else{
-                i++;
-            }
+            i++; // because pointer for s is always increased by one, doesn't matter the condition is satisfied or not. See detailed code above!
         }
-        return t.length()-j;
+        return t.length()-j;  
     }
 }
