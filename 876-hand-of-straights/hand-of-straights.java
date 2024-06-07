be67fbf0acd540d52933aece86c9fb0c108e9774
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-07 16:29:17 +0530

    Time: 50 ms (30.39%) | Memory: 44.5 MB (99.42%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -2,24 +2,27 @@ class Solution {
     public boolean  markNext(int groupSize,  int [] hand, int currele){
         int j = 0;
         int ct = 1;
+
         while (ct<groupSize && j<hand.length){
             if(hand[j]==-1){
                 j++;
             }
-            else if(hand[j]==currele+ct){
-                System.out.println(hand[j]);
 
+            else if(hand[j]==currele+ct){
                 ct++;
                 hand[j] = -1;
                 j++;
             }
+
             else{
                 j++;
             }
         }
+
         if (ct == groupSize){
             return true;
         }
+        
         return false;
     }
 
@@ -27,25 +30,29 @@ class Solution {
         if(hand.length % groupSize != 0){
             return false;
         }
+
         Arrays.sort(hand);
+
         int i = 0;
         while (i<hand.length){
             int currele = hand[i];
-            if (currele != -1){
-                System.out.println(hand[i]);
 
+            if (currele != -1){
                 hand[i] = -1;
                 if(!markNext(groupSize, hand, currele)){
                     return false;
                 }
             }
+
             i++;
         }
+
         for(int ele : hand){
             if(ele!=-1){
                 return false;
             }
         }
+
         return true;
     }
 }
