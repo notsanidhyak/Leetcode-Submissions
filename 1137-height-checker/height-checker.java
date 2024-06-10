e6d45d26d37a92bdfb9540e6325d78087a5acb2e
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-10 13:17:29 +0530

    Time: 2 ms (88.93%) | Memory: 41.3 MB (37.95%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int [] expected = new int [heights.length];
+        for(int i = 0; i<heights.length; i++) expected[i] = heights[i];
+        Arrays.sort(expected);
+        int ct = 0;
+        for(int i = 0; i<heights.length; i++){
+            if(expected[i]!=heights[i]) ct++;
+        }
+        return ct;    
+    }
+}
