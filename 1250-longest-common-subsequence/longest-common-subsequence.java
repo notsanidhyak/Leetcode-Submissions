7fb17226410941a920d93aa0685be03a26c3440a
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-11 11:58:34 +0530

    Time: 29 ms (19.22%) | Memory: 50.7 MB (61.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -19,20 +19,76 @@
 
 // --------------------------------------------------------------------
 
+// Memoization (DP) -
+
+// class Solution {
+//     public int dpfunc(int[][] dp, String text1, String text2, int i, int j){
+//         if(i==text1.length() || j==text2.length()){
+//             return 0;
+//         }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         if(text1.charAt(i)==text2.charAt(j)){
+//             dp[i][j] = 1 + dpfunc(dp, text1, text2, i+1, j+1);
+//             return dp[i][j];
+//         }
+//         else{
+//             dp[i][j]= Math.max(dpfunc(dp, text1, text2, i, j+1), dpfunc(dp, text1, text2, i+1, j));
+//             return dp[i][j];
+//         }
+//     }
+
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int [][] dp = new int [text1.length()][text2.length()];
+//         for (int i = 0; i<text1.length(); i++){
+//             for(int j = 0; j<text2.length(); j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         return dpfunc(dp, text1, text2, 0, 0);
+//     }
+// }
+
+// -------------------------------------------------------------------------
+
+// Brute Force (Recursion but i-1, j-1 concept)-
+
+// class Solution {
+//     public int rec(String text1, String text2, int i, int j){
+//         if(i<0 || j<0){
+//             return 0;
+//         }
+//         if(text1.charAt(i)==text2.charAt(j)){
+//             return 1 + rec(text1, text2, i-1, j-1); // match found
+//         }
+//         else {
+//             return Math.max(rec(text1, text2, i, j-1), rec(text1, text2, i-1, j)); // match not found, now here two cases possible, take what is beneficial for you (a match), hence Math.max()
+//         }
+//     }
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         return rec(text1, text2, text1.length()-1, text2.length()-1);
+//     }
+// }
+
+// -------------------------------------------------------------------------
+
+// Memoization (DP but with i-1, j-1 approach) -
+
 class Solution {
     public int dpfunc(int[][] dp, String text1, String text2, int i, int j){
-        if(i==text1.length() || j==text2.length()){
+        if(i<0 || j<0){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         if(text1.charAt(i)==text2.charAt(j)){
-            dp[i][j] = 1 + dpfunc(dp, text1, text2, i+1, j+1);
+            dp[i][j] = 1 + dpfunc(dp, text1, text2, i-1, j-1);
             return dp[i][j];
         }
         else{
-            dp[i][j]= Math.max(dpfunc(dp, text1, text2, i, j+1), dpfunc(dp, text1, text2, i+1, j));
+            dp[i][j]= Math.max(dpfunc(dp, text1, text2, i, j-1), dpfunc(dp, text1, text2, i-1, j));
             return dp[i][j];
         }
     }
@@ -44,6 +100,44 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        return dpfunc(dp, text1, text2, 0, 0);
+        return dpfunc(dp, text1, text2, text1.length()-1, text2.length()-1);
     }
 }
+
+// ------------------------------------------------------------------------
+
+// Tabulation Approach -
+
+// class Solution {
+//     public int dpfunc(int[][] dp, String text1, String text2, int i, int j){
+//         if(i==text1.length() || j==text2.length()){
+//             return 0;
+//         }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         if(text1.charAt(i)==text2.charAt(j)){
+//             dp[i][j] = 1 + dpfunc(dp, text1, text2, i+1, j+1);
+//             return dp[i][j];
+//         }
+//         else{
+//             dp[i][j]= Math.max(dpfunc(dp, text1, text2, i, j+1), dpfunc(dp, text1, text2, i+1, j));
+//             return dp[i][j];
+//         }
+//     }
+
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int [][] dp = new int [text1.length()+1][text2.length()+1];
+//         for (int i = 0; i<text1.length()+1; i++){
+//             for(int j = 0; j<text2.length()+1; j++){
+//                 if(i==0 || j==0){
+//                     dp[i][j]=0;
+//                 }
+//                 else if(text1.charAt(i) == text2.charAt(j)){
+//                     dp[i][j] = 1+dp[i-1][j-1];
+//                 }
+//             }
+//         }
+//         return dpfunc(dp, text1, text2, 0, 0);
+//     }
+// }
