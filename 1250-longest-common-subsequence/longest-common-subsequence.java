109fe078e0ccdba2b7649e1c2d161e4913aa8d1c
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-11 12:25:09 +0530

    Time: 19 ms (88.58%) | Memory: 41.4 MB (97.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -134,24 +134,64 @@
 
 // Tabulation Approach (But back from end approach) -
 
+// class Solution {
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int [][] dp = new int [text1.length()+1][text2.length()+1];
+//         for (int i = text1.length(); i>=0; i--){
+//             for(int j = text2.length(); j>=0; j--){
+//                 if(i==text1.length() || j==text2.length()){
+//                     dp[i][j]=0;
+//                 }
+//                 // note that dp's last row and col is the base case when nothing's there.
+//                 else if(text1.charAt(i) == text2.charAt(j)){
+//                     dp[i][j] = 1+dp[i+1][j+1];
+//                 }
+//                 else{
+//                     dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
+//                 }
+//             }
+//         }
+//         // access dp[0][0] basically when you are done with traversing the string from back
+//         return dp[0][0];
+//     }
+// }
+
+// ------------------------------------------------------------------------
+
+// Tabulation with space optimization -
+
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int [][] dp = new int [text1.length()+1][text2.length()+1];
-        for (int i = text1.length(); i>=0; i--){
-            for(int j = text2.length(); j>=0; j--){
-                if(i==text1.length() || j==text2.length()){
-                    dp[i][j]=0;
-                }
-                // note that dp's last row and col is the base case when nothing's there.
-                else if(text1.charAt(i) == text2.charAt(j)){
-                    dp[i][j] = 1+dp[i+1][j+1];
+        int [] prow = new int [text2.length()+1];
+        int [] crow = new int [text2.length()+1];
+        for(int i=0; i<text2.length()+1; i++){
+            crow[i]=0;
+            prow[i]=0;
+        }
+        for (int i = 1; i<=text1.length(); i++){
+            for(int j = 1; j<=text2.length(); j++){
+                // if(i==0 || j==0){
+                //     crow[j]=0;
+                // }
+                // note that pointer in text is moving one ahead hence doing i-1 here. also note that dp's row 0 and col 0 is the base case when nothing's there.
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    crow[j] = 1+prow[j-1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i][j+1], dp[i+1][j]);
+                    crow[j] = Math.max(crow[j-1], prow[j]);
                 }
             }
+            for (int k = 0; k<crow.length; k++){
+                prow[k] = crow[k];
+            }
         }
-        // access dp[0][0] basically when you are done with traversing the string from back
-        return dp[0][0];
+        // access last coloumn of crow/prow -> both same only!
+        return prow[text2.length()];
     }
 }
+
+// ------------------------------------------------------------------------
+
+// Tabulation with space optimization (But back from end approach) -
+
+// Can write similar code for this on same lines as above!
