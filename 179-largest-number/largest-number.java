0a5499f2ae858c1b1a0c0b52ff2f2f12619c0a40
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-06-12 17:13:19 +0530

    Time: 5 ms (96.94%) | Memory: 43 MB (69.20%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -8,6 +8,7 @@ class Solution {
 
         // main thing is here, you have to build a comparator which would compare s1 and s2 and return you the bigger (in terms of string);
         Comparator <String> mycomp = new Comparator<String>(){
+            // this is the logic for comparing using Arrays.sort would use to compare each pair of ele in array
             @Override
             public int compare(String s1, String s2){
                 String str1 = s1+s2;
@@ -18,7 +19,8 @@ class Solution {
 
         Arrays.sort(numAsS, mycomp);
 
-        if (numAsS[0].charAt(0) == '0') return "0";
+        // Edge case -
+        if (numAsS[0].charAt(0) == '0') return "0"; // 0 can come at index 0 only if all elements in num are 0, in that case, return a simple string "0"
 
         StringBuilder sb = new StringBuilder();
         for (String s : numAsS){
