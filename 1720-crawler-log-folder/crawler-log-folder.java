37ede6d3ac095fdfd017ddbc667b4356126e3264
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-07-10 22:12:25 +0530

    Time: 1 ms (99.42%) | Memory: 42 MB (51.62%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -2,14 +2,10 @@ class Solution {
     public int minOperations(String[] logs) {
         int level = 0;
         for (String i : logs){
-            if (i.charAt(0)!='.'){
-                level++;
-            }
+            if (i.charAt(0)!='.') level++;
             else{
                 if (i.charAt(1)=='.'){
-                    if (level>0){
-                        level--;
-                    }
+                    if (level>0) level--;
                 }
             }
         }
