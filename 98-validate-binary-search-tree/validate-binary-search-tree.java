17c1ec38126e3468080520b5849c724f2ed7f8e7
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-07-26 22:15:05 +0530

    Time: 70 ms (15.97%) | Memory: 45.2 MB (5.56%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,25 +13,56 @@
  *     }
  * }
  */
-class Solution {
-    public void inOrder(TreeNode root, List<Integer> ans){
-        if (root==null){
-            return;
-        }
-        inOrder(root.left, ans);
-        ans.add(root.val);
-        inOrder(root.right, ans);
-    }   
 
-    public boolean isValidBST(TreeNode root) {
-        // Based on the fact that inOrder traversal of BST is always increasing
-        List<Integer> ans = new ArrayList<>();
-        inOrder(root, ans);
-        for (int i = 0; i<ans.size()-1; i++){
-            if(ans.get(i)>=ans.get(i+1)){
-                return false;
-            }
+// ----------------------------------
+
+// Based on the fact that inOrder traversal of BST is always increasing
+
+// class Solution {
+//     public void inOrder(TreeNode root, List<Integer> ans){
+//         if (root==null){
+//             return;
+//         }
+//         inOrder(root.left, ans);
+//         ans.add(root.val);
+//         inOrder(root.right, ans);
+//     }   
+
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         inOrder(root, ans);
+//         for (int i = 0; i<ans.size()-1; i++){
+//             if(ans.get(i)>=ans.get(i+1)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+// ------------------------------------
+
+// Recursive approach
+
+class Solution {
+    public boolean check(TreeNode root, long low, long high){
+        if (root==null){
+            return true;
         }
-        return true;
+        // Imp to understand
+        System.out.println(low);
+        System.out.println(root.val);
+        System.out.println(high);
+        System.out.println();
+        if (root.val <= low || root.val >= high){
+
+            return false;
+        }
+        return check(root.left, low, root.val) && check(root.right, root.val, high);
+
+    }
+    public boolean isValidBST(TreeNode root) {
+        // Long is used for edge case, or else Integer also works!
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
