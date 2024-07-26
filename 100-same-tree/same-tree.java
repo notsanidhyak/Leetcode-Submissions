9b3f91b5637516d67962992274e3740b980e6e90
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-07-26 23:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (60.17%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -16,8 +16,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p==null && q==null) return true;
-        if (p==null || q==null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p==null || q==null) return false; // if both are null then above must have already returned true, if it is coming here, it means one of them is not null, hence return false.
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); // this node equal, left equal, right equal
     }
 }
