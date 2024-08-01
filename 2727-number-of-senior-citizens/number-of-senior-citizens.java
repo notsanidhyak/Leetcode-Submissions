53c9139477564701dff936d642d227a3b26012f5
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-08-01 23:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.64%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -2,6 +2,7 @@ class Solution {
     public int countSeniors(String[] details) {
         int ct = 0;
         for (String i: details){
+            // -48 to take care of ASCII, (int)'0' is 48 in ASCII.
             int x = (int)i.charAt(11)-48;
             int y = (int)i.charAt(12)-48;
             int age = (x*10)+y;
