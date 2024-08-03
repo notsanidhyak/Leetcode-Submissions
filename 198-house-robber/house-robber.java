f3b7e69858020e308966697d2f1dd28001857c97
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-08-03 22:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (12.79%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,18 @@
 class Solution {
-    public int rec(int[] nums, int previdx, int idx, int [][] dp){
-        if (idx == nums.length) return 0;
-        if (dp[previdx+1][idx]!=-1) return dp[previdx+1][idx];
-        int rob = 0;
-        if(previdx == -1 || previdx!=idx-1){
-            rob = nums[idx] + rec(nums, idx, idx+1, dp);
-        }
-        int notrob = 0 + rec(nums, previdx, idx+1, dp);
-        dp[previdx+1][idx] = Math.max(rob, notrob);
-        return dp[previdx+1][idx];
+    public int rec(int[] nums, int idx, int [] dp){
+        if (idx >= nums.length) return 0;
+        if (dp[idx]!=-1) return dp[idx];
+        int rob = nums[idx] + rec(nums, idx+2, dp);
+        int notrob = 0 + rec(nums, idx+1, dp);
+        dp[idx] = Math.max(rob, notrob);
+        return dp[idx];
     }
 
     public int rob(int[] nums) {
-        int [][] dp = new int [nums.length][nums.length];
+        int [] dp = new int[nums.length];
         for(int i = 0; i<nums.length; i++){
-            for(int j = 0; j<nums.length; j++){
-                dp[i][j] = -1;
-            }
+            dp[i] = -1; 
         }
-        return rec(nums, -1, 0, dp);
+        return rec(nums, 0, dp);
     }
 }
