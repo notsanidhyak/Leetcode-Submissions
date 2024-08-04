78af4d4fefe554ee35f6c9d04833895689a691c5
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-08-04 16:38:43 +0530

    Time: 180 ms (21.62%) | Memory: 70.5 MB (18.47%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,13 +1,17 @@
+// Brute Force Approach -
+
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         ArrayList<Integer> arr = new ArrayList<>();
         for(int i = 0; i<nums.length; i++){
+            int sum = 0;
             for(int j = i; j<nums.length; j++){
-                int sum = 0;
-                for(int k = i; k<=j; k++){
-                    sum+=nums[k];
-                }
+                sum += nums[j];
                 arr.add(sum);
+                // for(int k = i; k<=j; k++){
+                //     sum+=nums[k];
+                // }
+                // arr.add(sum);
             }
         }
         Collections.sort(arr);
@@ -19,3 +23,14 @@ class Solution {
         return ans;
     }
 }
+
+// -----------------------------------------------------------------------
+
+// Optimized Approach -
+
+// class Solution {
+//     public int rangeSum(int[] nums, int n, int left, int right) {
+
+        
+//     }
+// }
