62f375bff357968671b10ac2045f2afd1ce058b7
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-09-17 05:42:31 +0530

    Time: 2 ms (100.00%) | Memory: 42.1 MB (54.60%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -5,21 +5,13 @@ class Solution {
        ArrayList<String> al = new ArrayList<>(); 
        HashMap<String,Integer> hm = new HashMap<>();
        HashMap<String,Integer> hm1 = new HashMap<>();
-       for(String sh : arr){
-        hm.put(sh,hm.getOrDefault(sh,0)+1);
-       }
-       for(String sh : arr1){
-        hm1.put(sh,hm1.getOrDefault(sh,0)+1);
-       }
+       for(String sh : arr) hm.put(sh,hm.getOrDefault(sh,0)+1);
+       for(String sh : arr1) hm1.put(sh,hm1.getOrDefault(sh,0)+1);
        for(Map.Entry<String,Integer> entry : hm.entrySet()){
-        if( entry.getValue() ==1 && !(hm1.containsKey(entry.getKey()))){
-            al.add(entry.getKey());
-        }
+        if( entry.getValue() ==1 && !(hm1.containsKey(entry.getKey()))) al.add(entry.getKey());
        }
        for(Map.Entry<String,Integer> entry : hm1.entrySet()){
-        if( entry.getValue() ==1 && !(hm.containsKey(entry.getKey()))){
-            al.add(entry.getKey());
-        }
+        if( entry.getValue() ==1 && !(hm.containsKey(entry.getKey()))) al.add(entry.getKey());
        }
     String[] array = al.toArray(new String[0]);
     return array;
