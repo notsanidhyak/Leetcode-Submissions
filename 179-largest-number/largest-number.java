6847ab27919a6c37c00eb1d9a5ad9671f8e7017c
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-09-19 01:50:54 +0530

    Time: 5 ms (98.15%) | Memory: 43 MB (76.06%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,32 +1,11 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        if (nums.length == 0) return "";
-        String [] numAsS = new String[nums.length];
-        for(int i = 0; i<nums.length; i++){
-            numAsS[i] = String.valueOf(nums[i]);
-        }
-
-        // main thing is here, you have to build a comparator which would compare s1 and s2 and return you the bigger (in terms of string);
-        Comparator <String> mycomp = new Comparator<String>(){
-            // this is the logic for comparing using Arrays.sort would use to compare each pair of ele in array
-            @Override // here we are overriding the compare func inside the Comparator class bcs we want our func here!
-            public int compare(String s1, String s2){
-                String str1 = s1+s2;
-                String str2 = s2+s1;
-                return str2.compareTo(str1);
-            }
-        };
-
-        Arrays.sort(numAsS, mycomp);
-
-        // Edge case -
-        if (numAsS[0].charAt(0) == '0') return "0"; // 0 can come at index 0 only if all elements in num are 0, in that case, return a simple string "0"
-
-        StringBuilder sb = new StringBuilder();
-        for (String s : numAsS){
-            sb.append(s);
-        }
-
-        return sb.toString();
+        String[] array =  new String[nums.length];
+        for(int i=0; i<nums.length; i++) array[i] = String.valueOf(nums[i]);
+        Arrays.sort(array,(a,b)-> (b+a).compareTo(a+b));
+        if(array[0].equals("0")) return "0";
+        StringBuilder largest = new StringBuilder();
+        for(int i=0; i<array.length; i++) largest.append(array[i]);
+        return largest.toString();
     }
 }
