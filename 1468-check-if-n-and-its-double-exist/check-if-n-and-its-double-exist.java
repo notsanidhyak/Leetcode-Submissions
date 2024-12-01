1ef327eb2778968bba5424f3a5458523fda02014
sanidhyak <86651116+notsanidhyak@users.noreply.github.com>
2024-12-01 16:44:34 +0530

    Time: 2 ms (93.52%) | Memory: 43 MB (33.23%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>(); // HashMap to store elements
-
         for (int num : arr) { // Iterate over the array
             // Check if 2 * num or num / 2 exists in the map
             if (map.containsKey(2 * num) || (num % 2 == 0 && map.containsKey(num / 2))) {
@@ -10,7 +9,6 @@ class Solution {
             // Add the current number to the map
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
         return false; // Return false if no such pair exists
     }
 }
